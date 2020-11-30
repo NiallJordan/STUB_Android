596a3d39ae55f5455b02b2dd2eccbd938ecdf825
NiallJordan <niall.jordan2012@gmail.com>
2020-11-30 12:02:47 +0000

    Navigation Working

--- app/src/main/java/com/wit/stub/MainActivity.kt
+++ app/src/main/java/com/wit/stub/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.wit.stub
 
+import android.accounts.Account
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.fragment.app.Fragment
+import com.wit.stub.fragments.AccountFragment
+import com.wit.stub.fragments.HomeFragment
+import com.wit.stub.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val home = HomeFragment()
+        val account = AccountFragment()
+        val settings = SettingsFragment()
+
+        setMainFragment(home)
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> setMainFragment(home)
+                R.id.settings -> setMainFragment(settings)
+                R.id.account -> setMainFragment(account)
+            }
+            true
+        }
     }
+
+    private fun setMainFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.wrapper, fragment)
+            commit()
+        }
 }
